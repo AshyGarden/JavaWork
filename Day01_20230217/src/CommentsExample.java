5cba24056f05092eeaf5a87aa726c68c6b1468c2
YJ <jun940317@gmail.com>
2023-02-17 17:09:04 +0900

    Day1_20230217

---
+++ Day01_20230217/src/CommentsExample.java
@@ -0,0 +1,33 @@
+/** (/**)  
+ * 문서생성용 주석
+ * 배포용 문서에 설명을 달아놓을때 사용합니다.
+ * @author ict03-25
+ * @version 1.0
+ * @since 2023.02.17~
+ */
+
+
+//이 파일은 주석을 연습하는 파일입니다.
+public class CommentsExample {
+		
+	public static void main(String[] args) {
+		/*
+		#main 함수(매서드)는 프로그램 실행시 자동으로 동작하는 함수이며, 
+		반드시 필요한 함수이다.
+		
+		#자바코드를 해석하여 실행시키는 자바 가상 머신(JVM)은 프로그램 실행시 
+		자동으로 main함수를 불러서 내부의 코드를 실행시킵니다.
+		*/
+		
+		//syso + ctrl + space    빠른 출력함수를 생성
+		//ctrl + F11             실행 단축키
+		//ctrl +  /              주석 처리 or 해제(원하는 문장에 커서 배치 후 사용)
+		//ctrl + alt  + 밑방향키   드래그 영역 밑으로복사
+		//ctrl + shift+ /        선택영역 주석
+		
+		 System.out.println("Hello Java!");
+		 System.out.println("Hello World~"); 
+
+		
+	}
+}
